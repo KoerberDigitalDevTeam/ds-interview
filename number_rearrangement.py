174c6a1f8092e59370c1a935c6180a621dffe0d0
davidbreyel <david.breyel@connyun.com>
2020-02-12 10:06:31 +0100

    created number rearrangement problem

---
+++ number_rearrangement.py
@@ -0,0 +1,18 @@
+"""
+- Please, write a function that takes an integer as an input and prints its digits in ascending order
+  (examples are given below)
+  a) Please, do not use a built-in sorting function
+  b) Feel free to use a  built-in sorting function
+- Adjust both your functions such that every digit appears at most once in the result
+
+
+Examples:
+    - First problem
+      rearrange_digits(4323231) = 1223334
+      rearrange_digits(10070689) = 00016789
+      rearrange_digits(2525) = 2255
+    - Second problem
+      rearrange_digits(4323231) = 1234
+      rearrange_digits(10070689) = 016789
+      rearrange_digits(2525) = 25
+"""
