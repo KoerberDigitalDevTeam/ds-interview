52ccce37902162268b58eaa51ee179ca7c1c6f61
davidbreyel <david.breyel@connyun.com>
2020-02-12 08:40:49 +0100

    created simple problem

---
+++ fizz_buzz_variation.py
@@ -0,0 +1,9 @@
+"""
+This problem is a variant of the famous fizz-buzz problem. This version is taken from the first problem
+of projecteuler.net.
+
+- Please, find the sum of all multiples of 3 and 5 below N, take N=1000 so start with. Hint: for N=10 the
+  solution is 23.
+- What is the complexity of your code, how about memory consumption?
+- Can you think of a more/less performant version with respect to memory/complexity?
+"""
