db5b89a64b90cdf1570ad5fbcb8a05d9288ec75b
Bastian Kronenbitter <bastian.kronenbitter@koerber.digital>
2020-02-14 15:35:42 +0100

    change problem description

--- number_rearrangement.py
+++ number_rearrangement.py
@@ -1,18 +1,15 @@
 """
-- Please, write a function that takes an integer as an input and prints its digits in ascending order
-  (examples are given below)
-  a) Please, do not use a built-in sorting function
-  b) Feel free to use a  built-in sorting function
-- Adjust both your functions such that every digit appears at most once in the result
+Please, write a function that takes an integer as an input and prints its digits in ascending order
+(examples are given below)
+-- Feel free to approach the problem in any way
+-- Can you do it without casting the inital number to a string?
+---- For a number of fixed length
+---- For a number of arbitrary length
+-- Can you do it without using any kind of sorting or comparison?
 
 
 Examples:
-    - First problem
-      rearrange_digits(4323231) = 1223334
-      rearrange_digits(10070689) = 00016789
-      rearrange_digits(2525) = 2255
-    - Second problem
-      rearrange_digits(4323231) = 1234
-      rearrange_digits(10070689) = 016789
-      rearrange_digits(2525) = 25
+  rearrange_digits(4323231) = 1223334
+  rearrange_digits(10070689) = 00016789
+  rearrange_digits(2525) = 2255
 """
